6e0d7d58e3c0242721bc6b3c09d7d07b3401603f
Jack Dolgin <jack.dolgin@duke.edu>
2019-10-08 18:15:03 -0400

    Update script.py
    -voice duration getting close to desire timing
    
    -replace TextStim with TextBox as default text presenter
    
    -adjust size of TextBox for each trial to correspond with the length of the presented word

--- script.py
+++ script.py
@@ -23,10 +23,17 @@ win = visual.Window(
     size=(1280, 800), fullscr=True, allowGUI=False,
     monitor='testMonitor', color=[1,1,1], useFBO=True)
 
-#pics_cap = 150
-pics_cap = 3
+pics_cap = 200
+
 framelength = win.monitorFramePeriod
-timeout = int(round(2.5/framelength))
+def to_frames(t):                                                               # Converts time to frames accounting for the computer's refresh rate (aka framelength); input is the desired time on screen, but the ouput is the closest multiple of the refresh rate
+    return int(round(t / framelength))
+
+timeout_input = 3
+timeout = to_frames(timeout_input)
+resp_timeout_input = 1
+resp_timeout = to_frames(resp_timeout_input)
+fix_duration = to_frames(.5)
 
 pics_info = pd.read_csv('IPNP_spreadsheet.csv')
 
@@ -43,49 +50,47 @@ pics_info_dplyed = (pics_info >>
     arrange(X.Agreement_Factor, X.Alternative_Names, X.Mean_RT_Dominant) >>
     head(pics_cap))
 
+fix = visual.TextStim(win=win, name = 'fix', color='black',text='+')
+
 adict = dict([(i.capitalize(), (visual.ImageStim(win, size=[0.5,0.5], image = os.path.join("IPNP_Pictures", a + i + ".png")),
-                                visual.TextStim(win, text = i.capitalize(), color=(-1,-1,-1),fontFiles=[os.path.join('fonts', 'Lato-Reg.ttf')])))  for i, a in zip(pics_info_dplyed.Dominant_Response, pics_info_dplyed.Pic_Num)])
-#                                visual.TextBox(window = win, text = i.capitalize(), font_color=(-1,-1,-1), background_color = [1,1,1,.5], size = (.3,.3))))  for i, a in zip(pics_info_dplyed.Dominant_Response, pics_info_dplyed.Pic_Num)])
+#                                visual.TextStim(win, text = i.upper(), color=(-1,-1,-1),fontFiles=[os.path.join('fonts', 'Lato-Reg.ttf')])))  for i, a in zip(pics_info_dplyed.Dominant_Response, pics_info_dplyed.Pic_Num)])
+                                visual.TextBox(window = win, text = i.upper(), font_color=(-1,-1,-1), background_color = [1,1,1,.8], textgrid_shape=[len(i), 1])))  for i, a in zip(pics_info_dplyed.Dominant_Response, pics_info_dplyed.Pic_Num)])
 
 
 def stim_onset(logical, sub_key):
     [adict[sub_key][i].setAutoDraw(logical) for i in range(len(adict[sub_key]))]
     
 def runTrial(a_key):
-    frame_count = 0
-    stim_onset(True, a_key)
+    fix_count = frame_count = resp_frame_count = 0
+    while fix_count < fix_duration:
+        fix.draw()
+        fix_count += 1
+        win.flip()
+#    stim_onset(True, a_key)
     vpvkOff = vk.OffsetVoiceKey()
     vpvkOn = vk.OnsetVoiceKey(
-        sec=2, 
+        sec = timeout_input,
         file_out = os.path.join("data", a_key + ".wav"))
-    vpvkOff.start()
-    vpvkOn.start()
+    [i.start() for i in (vpvkOff, vpvkOn)]
     trialClock.reset()
-    while frame_count < timeout:
-        if event.getKeys(keyList = ["escape"]):
-            core.quit()
-#        [adict[a_key][i].draw() for i in range(len(adict[a_key]))]
-        frame_count += 1
-        win.flip()
+    while frame_count < timeout and resp_frame_count < resp_timeout:
+        if event.getKeys(keyList = ["escape"]): core.quit()
+        [adict[a_key][i].draw() for i in range(len(adict[a_key]))]
         if hasattr(vpvkOff, 'event_offset') and vpvkOff.event_offset > 0:
-            break
-    stim_onset(False, a_key)
+            resp_frame_count += 1 
+        win.flip()
+#    stim_onset(False, a_key)
     vpvkOff.stop()
     vpvkOn.stop()
-    print (vpvkOn.event_onset)
 
-
-
-
-
-    vpvk2 = vk.OffsetVoiceKey(file_in=os.path.join("data", a_key + ".wav"))
-    vpvk2.start()  # non-blocking
-    frame_count = 0
-    while frame_count < 100:
-        if event.getKeys(keyList = ["escape"]):
-            core.quit()
-        frame_count += 1
-        win.flip()
+#    vpvk2 = vk.OffsetVoiceKey(file_in=os.path.join("data", a_key + ".wav"))
+#    vpvk2.start()  # non-blocking
+#    frame_count = 0
+#    while frame_count < 100:
+#        if event.getKeys(keyList = ["escape"]):
+#            core.quit()
+#        frame_count += 1
+#        win.flip()
 
 for key in adict:
     runTrial(key)
