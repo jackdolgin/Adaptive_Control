63f5b2a87045c39a3f8e2b8abf7a1c7933197040
Jack Dolgin <jack.dolgin@duke.edu>
2019-10-09 00:49:07 -0400

    Update script.py
    in the process of adding incongruent trials, these commits haven't been run yet so there might be errors

--- script.py
+++ script.py
@@ -8,6 +8,7 @@ from psychopy import locale_setup, prefs, gui, visual, core, data, event, loggin
 import numpy as np
 import pandas as pd
 from dfply import *
+import random
 import glob
 import os
 import psychopy.voicekey as vk
@@ -24,16 +25,21 @@ win = visual.Window(
     monitor='testMonitor', color=[1,1,1], useFBO=True)
 
 pics_cap = 200
+pct_split = .75
+bigger_group = round(pics_cap * pct_split)
+two_groups = random.sample((bigger_group, pics_cap - bigger_group), 2)
 
 framelength = win.monitorFramePeriod
 def to_frames(t):                                                               # Converts time to frames accounting for the computer's refresh rate (aka framelength); input is the desired time on screen, but the ouput is the closest multiple of the refresh rate
     return int(round(t / framelength))
 
-timeout_input = 3
+fix_duration_input = .5
+fix_duration = to_frames(.5)
+timeout_input = 3 + fix_duration_input
 timeout = to_frames(timeout_input)
 resp_timeout_input = 1
 resp_timeout = to_frames(resp_timeout_input)
-fix_duration = to_frames(.5)
+
 
 pics_info = pd.read_csv('IPNP_spreadsheet.csv')
 
@@ -41,45 +47,43 @@ allpics = os.listdir("IPNP_Pictures")
 
 trialClock = core.Clock()
 
-
 def filter_pics(series_element):
     return any(s.startswith(series_element) for s in allpics)
 
 pics_info_dplyed = (pics_info >>
     mask(X.Pic_Num.apply(filter_pics)) >>
     arrange(X.Agreement_Factor, X.Alternative_Names, X.Mean_RT_Dominant) >>
-    head(pics_cap))
+    head(pics_cap + bigger_group) >>
+    sample(frac = 1) >>
+    mutate(Lag_Dominant_Response = lag(X.Dominant_Response, bigger_group)))
 
 fix = visual.TextStim(win=win, name = 'fix', color='black',text='+')
 
-adict = dict([(i.capitalize(), (visual.ImageStim(win, size=[0.5,0.5], image = os.path.join("IPNP_Pictures", a + i + ".png")),
-#                                visual.TextStim(win, text = i.upper(), color=(-1,-1,-1),fontFiles=[os.path.join('fonts', 'Lato-Reg.ttf')])))  for i, a in zip(pics_info_dplyed.Dominant_Response, pics_info_dplyed.Pic_Num)])
-                                visual.TextBox(window = win, text = i.upper(), font_color=(-1,-1,-1), background_color = [1,1,1,.8], textgrid_shape=[len(i), 1])))  for i, a in zip(pics_info_dplyed.Dominant_Response, pics_info_dplyed.Pic_Num)])
 
+adict = {}
+for i, a, b in zip(pics_info_dplyed.Dominant_Response, pics_info_dplyed.Pic_Num, range(pics_cap)):
+    adict[i.capitalize()] = (visual.ImageStim(win, size=[0.5,0.5], image = os.path.join("IPNP_Pictures", a + i + ".png")),
+                            visual.TextBox(window = win, text = i.upper(), font_color=(-1,-1,-1), background_color = [1,1,1,.8], textgrid_shape=[len(i), 1]))
 
-def stim_onset(logical, sub_key):
-    [adict[sub_key][i].setAutoDraw(logical) for i in range(len(adict[sub_key]))]
-    
 def runTrial(a_key):
-    fix_count = frame_count = resp_frame_count = 0
-    while fix_count < fix_duration:
-        fix.draw()
-        fix_count += 1
-        win.flip()
-#    stim_onset(True, a_key)
-    vpvkOff = vk.OffsetVoiceKey()
-    vpvkOn = vk.OnsetVoiceKey(
-        sec = timeout_input,
-        file_out = os.path.join("data", a_key + ".wav"))
-    [i.start() for i in (vpvkOff, vpvkOn)]
-    trialClock.reset()
+    frame_count = resp_frame_count = 0
+    fix.setAutoDraw(True)
     while frame_count < timeout and resp_frame_count < resp_timeout:
         if event.getKeys(keyList = ["escape"]): core.quit()
-        [adict[a_key][i].draw() for i in range(len(adict[a_key]))]
-        if hasattr(vpvkOff, 'event_offset') and vpvkOff.event_offset > 0:
-            resp_frame_count += 1 
+        if frame_count == fix_duration:
+            fix.setAutoDraw(False)
+            vpvkOff = vk.OffsetVoiceKey()
+            vpvkOn = vk.OnsetVoiceKey(
+                sec = timeout_input,
+                file_out = os.path.join("data", a_key + ".wav"))
+            [i.start() for i in (vpvkOff, vpvkOn)]
+            trialClock.reset()
+        elif fix_duration < frame_count < timeout and resp_frame_count < resp_timeout:
+            [adict[a_key][i].draw() for i in range(len(adict[a_key]))]
+            if hasattr(vpvkOff, 'event_offset') and vpvkOff.event_offset > 0:
+                resp_frame_count += 1
+        frame_count += 1
         win.flip()
-#    stim_onset(False, a_key)
     vpvkOff.stop()
     vpvkOn.stop()
 
